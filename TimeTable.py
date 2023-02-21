bd16e1fd4d7fc78be03cd1deb1d038d2e9b98ed2
Guest-Liang <GuestLiang@outlook.com>
2023-02-21 15:15:46 +0800

    entire update

--- TimeTable.py
+++ TimeTable.py
@@ -61,20 +61,25 @@ print("正在处理，请稍等")
 StartDay=datetime.date(2023, 2, 20)
 
 
-
-#按行拆分单元格
+'''#按行拆分单元格
 BR=GetElementIndex("\n", Sheet['C4'].value)#获取换行符位置
 for i in range(len(BR)-1):
-    print(Sheet['C4'].value[BR[i]:BR[i+1]], end="")
+    print(Sheet['C4'].value[BR[i]:BR[i+1]], end="")'''
 
 
 
 
-'''
-for row in range(4:17):
-    for column in range(2:8):
-        for DailyCount in GetElementIndex("\n", Sheet):
-'''
+
+
+
+for row in range(4, 18):
+    for column in range(2, 9):
+        CellBR=GetElementIndex("\n", Sheet.cell(row, column).value)
+        for i in range(len(CellBR)-1):
+            print(Sheet.cell(row, column).value[CellBR[i]:CellBR[i+1]], end="")
+        print(f"\n----------第{row}行第{column}列课程拆分-----------")
+
+
 
 
 
